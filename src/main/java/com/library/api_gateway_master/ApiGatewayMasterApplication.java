bd1cf518b9e1ef8e3761c16e99c3d4229adc1766
athidileep <140885247+athidileep@users.noreply.github.com>
2024-11-20 19:57:56 +0530

    Add files via upload

---
+++ src/main/java/com/library/api_gateway_master/ApiGatewayMasterApplication.java
@@ -0,0 +1,13 @@
+package com.library.api_gateway_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/api_gateway_master/ApiGatewayMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.api_gateway_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
